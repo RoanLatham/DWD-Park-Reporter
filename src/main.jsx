c726fe0efa9ada8a8910b2c883ff266181378aa9
Roan <roanlatham@gmail.com>
2024-02-15 16:31:22 +0000

    added service worker to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+  navigator.serviceWorker
+  .register("./service-worker.js")
+  .then((registration) => {
+  console.log("Service Worker registered! Scope: ", registration.scope);
+  })
+  .catch((err) => {
+  console.log("Service Worker registration failed: ", err);
+  });
+  });
+ }
+
 const DATA = JSON.parse(localStorage.getItem('tasks')) || [];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
