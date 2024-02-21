47acafe7853a5aa8593d087f1c3670dc12deff70
Roan <roanlatham@gmail.com>
2024-02-21 20:29:57 +0000

    Location API calls implemented

--- src/main.jsx
+++ src/main.jsx
@@ -26,6 +26,34 @@ if ("serviceWorker" in navigator) {
  });
 }
 
+const geoFindMe = () => {
+  if (!navigator.geolocation) {
+    console.log("Geolocation is not supported by your browser");
+  } else {
+    console.log("Locating…");
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+};
+
+const success = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log(latitude, longitude);
+  console.log(`Latitude: ${latitude}°, Longitude: ${longitude}°`);
+  console.log(
+    `Try here: https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`
+  );
+  locateTask(lastInsertedId, {
+    latitude: latitude,
+    longitude: longitude,
+    error: "",
+  });
+};
+
+const error = () => {
+  console.log("Unable to retrieve your location");
+};
+
 const DATA = JSON.parse(localStorage.getItem('tasks')) || [];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
